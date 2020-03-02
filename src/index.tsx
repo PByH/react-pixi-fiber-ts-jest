35e28003a01b898ecb120b5e7e3a0bf3d22ac5cd
pbyh <pbyh_kpyt@mail.ru>
2020-03-02 20:58:34 +0700

    pixi global added (pixi inspector support)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
+import * as PIXI from "pixi.js";
 import App from "./App";
+
+window.PIXI = PIXI;
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
